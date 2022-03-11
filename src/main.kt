ff38214112ef70c1921d6a12877d68ec89603a5c
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-03-11 19:31:37 +0300

    Add my file

--- src/main.kt
+++ src/main.kt
@@ -15,7 +15,7 @@ fun grammar(){
     }
 }
 fun integers(){
-    var numbers = arrayOf( 17,4,213,78,43,90,31,3,73,11,158,62)
+    var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
     var sum = (numbers[1]+numbers[4])
     println(sum)
     println(numbers.indexOf(158))
